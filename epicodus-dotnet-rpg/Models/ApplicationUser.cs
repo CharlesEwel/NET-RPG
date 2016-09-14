103bfbaeb26bd699b9a021004801f5933acf7767
Charles Ewel and Ryan Streur <charles+ryan@>
2016-09-14 08:55:45 -0700

    add a basic user authentication form

---
+++ epicodus-dotnet-rpg/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace epicodus_dotnet_rpg.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
